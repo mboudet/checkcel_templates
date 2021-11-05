fe38253f3e5bf8bf2a179ffcc9a5c18ee42bb1a6
mboudet <mateo.boudet@gmail.com>
2021-11-05 15:47:10 +0100

    Update 2

--- DeepImpact/DeepImpact_plot_survey_field.py
+++ DeepImpact/DeepImpact_plot_survey_field.py
@@ -1,15 +1,15 @@
 from checkcel import Checkplate
-from checkcel.validators import SetValidator, NoValidator
+from checkcel.validators import SetValidator, NoValidator, DateValidator
 from collections import OrderedDict
 
 
 class MyTemplate(Checkplate):
     validators = OrderedDict([
+        ("Agricultural field", NoValidator()),
         ("Plot", SetValidator(valid_values=['1', '2', '3', '4', 'mixed'])),
         ("Weed Species", NoValidator()),
         ("Density", SetValidator(valid_values=['+', '1', '2', '3-', '3+', '4', '5', '6'])),
         ("Phenological stage", SetValidator(valid_values=['A', 'B', 'C', 'D', 'E'])),
-        ("Agricultural field", NoValidator()),
-        ("Survey date", NoValidator()),
+        ("Survey date", DateValidator()),
         ("Session", NoValidator()),
     ])
